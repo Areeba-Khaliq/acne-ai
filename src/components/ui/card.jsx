96af22cbfb0211dafa74ea7a29a6bf7badafd373
Areeba Khaliq <areebakhaliqrandhawa@gmail.com>
2026-06-03 12:35:12 +0500

    chore: backend only

--- src/components/ui/card.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Card({ children }) {
-  return <div className="shadow-lg p-4 rounded bg-white">{children}</div>;
-}
-
-export function CardContent({ children }) {
-  return <div className="p-2">{children}</div>;
-}
